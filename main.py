dc1af8b955aaf453d491e315ee0361aac29220af
ydvsatyammm <satyamyadav64191@gmail.com>
2026-05-03 07:08:03 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Satyam's First AI Project
+print("------------------------------------")
+print("Hello World! I am Satyam.")
+print("B.Tech CSE AI (1st Year)")
+print("My GitHub journey starts here!")
+print("------------------------------------")
